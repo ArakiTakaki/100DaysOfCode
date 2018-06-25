59dc3326d0561647c504240e8a38a357f0ab4612
TakakiAraki <araki_takaki@yahoo.co.jp>
2018-06-25 22:55:31 +0900

    ディレクトリの作成とテンプレートエンジンの読み込みが成功した。

--- 001/commands/foundation.go
+++ 001/commands/foundation.go
@@ -3,13 +3,13 @@ package commands
 import (
 	"errors"
 	"fmt"
+	"html/template"
+	"os"
 
 	"github.com/urfave/cli"
 )
 
-/**
- * 1 DBName
- */
+// Foundation テスト
 func Foundation() cli.Command {
 	foundation := cli.Command{
 		Name:   "foundation",
@@ -20,11 +20,35 @@ func Foundation() cli.Command {
 }
 
 func action(c *cli.Context) error {
-	test := c.Args()
-	if len(test) < 3 {
+	args := c.Args()
+	if len(args) < 3 {
 		return errors.New("値が不正です。")
 	}
-	// サンプル
-	fmt.Println("type " + test[0] + " struct{\n  gorm.Model\n  " + test[1] + ": " + test[2] + "\n}")
+
+	table := args[0]
+	// record := args[1:]
+
+	// テンプレートファイルの選択
+	tmp, err := template.ParseFiles("./template/foundation.go.tmpl")
+
+	// ディレクトリの生成
+	os.MkdirAll("./model", os.ModeDir)
+	os.MkdirAll("./db", os.ModeDir)
+
+	// ファイルの作成
+	file, err := os.Create("./model/" + table + ".go")
+	if err != nil {
+		fmt.Println("ディレクトリの作成に失敗しました。")
+		panic(err)
+	}
+
+	// ファイルの書き込み
+	err = tmp.Execute(file, nil)
+
+	if err != nil {
+		fmt.Println("ファイルの書き込みに失敗しました。")
+		panic(err)
+	}
+
 	return nil
 }
